5fb37af641d744f25eabb485ce749acb0f916d35
romjul77 <surajbhatt54321@gmail.com>
2024-08-03 19:56:34 +0530

    refactor Shop component to use state for inventory

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,16 +1,26 @@
-import InventoryList from "../../components/InventoryList/InventoryList"
-import { inventoryData } from "../../data/data"
+// npm modules
+import { useState } from "react"
 
+// components
+import InventoryList from "../../components/InventoryList/InventoryList"
+
+// data
+import { inventoryData } from "../../data/data"
+ 
 const Shop = () => {
+    
+    const [inventory, setInventory] = useState(inventoryData)
     console.log('Inventory Data:', inventoryData)
+
     return (
         <main>
             <h1>Shop</h1>
             <ul>
-               <InventoryList inventory = {inventoryData} />
+               <InventoryList inventory = {inventory} />
             </ul>
         </main>
     )
 }
-
+ 
 export default Shop
+   
