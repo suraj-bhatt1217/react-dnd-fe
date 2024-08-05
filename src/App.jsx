ab146592d22494833d1f9df9aa274d79944a6fb7
romjul77 <surajbhatt54321@gmail.com>
2024-08-05 14:17:18 +0530

    add navbar and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+//components
+import NavBar from './components/NavBar/NavBar'
+
 //pages
 import Shop from './pages/Shop/Shop'
 import './App.css'
  
 function App() {
   return (
-    <Shop />
+    <>
+      <NavBar />
+      <Shop />
+    </>
   )
 }
   
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+// npm modules
+import { Link } from "react-router-dom";
+
+//css
+import './NavBar.css'
+
+// assets
+import reactLogo from '../../assets/react.svg'
+
+const NavBar = () => {
+    return ( 
+        <header className="navbar">
+            <img src={reactLogo} alt="The React logo" className="logo" />
+            <nav className="nav-links">
+                <Link to='/shop'>Shop</Link>
+                <Link to='/monsters'>Scary Monsters</Link>
+                <Link to='/spells'>Search for Spells</Link>
+            </nav>
+        </header>
+     )
+}
+export default NavBar
